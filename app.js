95702d9ac6e1791abc187d32b706e7e557a08cfb
kierepkae <kierepkaedyta@gmail.com>
2019-10-19 13:09:20 +0100

    try to console log params

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ app.listen(3000, () => {
   console.log('listening at 3000');
 });
 
-app.get('/', (req, res) => {
-  console.log(req.protocol);
-  console.log(req.secure);
-})
+// app.get('/', (req, res) => {
+//   console.log(req.protocol);
+//   console.log(req.secure);
+// })
+
+
+app.all('/', (req) => {
+  console.log(req.query);
+});
