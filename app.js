97d696775c70663276d0a7dbc646a970c79e6147
kierepkae <kierepkaedyta@gmail.com>
2019-10-19 18:12:55 +0100

    define response on root path

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
+  res.write('hello world');
+  res.end();
 })
-app.get('/hello/new-user', (req, res) => {
-  console.log('add new user');
-});
+// app.get('/hello/new-user', (req, res) => {
+//   console.log('add new user');
+// });
 
 
-app.get('/hello/:name', (req, res) => {
-  console.log('hello user');
-});
+// app.get('/hello/:name', (req, res) => {
+//   console.log('hello ' + req.params.name);
+// });
 
 
