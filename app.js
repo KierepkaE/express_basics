1097009fba9f4c163cec4802a0ce2d3cf9f9e92c
kierepkae <kierepkaedyta@gmail.com>
2019-10-19 12:41:23 +0100

    create directory static and index.html

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const app = express();
 app.listen(3000, () => {
   console.log('listening at 3000');
 });
+
+app.get('/', (req, res) => {
+  console.log(req.protocol);
+  console.log(req.secure);
+})
