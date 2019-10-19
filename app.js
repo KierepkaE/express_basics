a7dac4fe9e97692b2326e0ad93667176ac9a8d5b
kierepkae <kierepkaedyta@gmail.com>
2019-10-19 13:37:59 +0100

    create routes for /user.new-user and /user/:name

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 app.listen(3000, () => {
-  console.log('listening at 3000');
+  console.log('listening . . . ');
 });
 
-// app.get('/', (req, res) => {
-//   console.log(req.protocol);
-//   console.log(req.secure);
-// })
-
-
-app.all('/', (req) => {
-  console.log(req.query);
+app.get('/', (req, res) => {
+})
+app.get('/hello/new-user', (req, res) => {
+  console.log('add new user');
 });
+
+
+app.get('/hello/:name', (req, res) => {
+  console.log('hello user');
+});
+
+
