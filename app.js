fe5e83d1c377a12161c495a04cf5900ae8832396
kierepkae <kierepkaedyta@gmail.com>
2019-10-20 14:19:21 +0100

    save name in session cookie

--- app.js
+++ app.js
@@ -5,31 +5,17 @@ app.listen(3000, () => {
   console.log('listening . . . ');
 });
 
+
+
 app.get('/', (req, res) => {
-  res.send(`<!DOCTYPE html>
-<html>
-<body>
-<img src='/logo'>
-</body>
-</html>
 
-`
-  )
-});
-
-app.get('/logo', (req, res) => {
-  const fileName = path.join(__dirname, 'static/cat.jpg')
-  res.download(fileName, 'kitty');
-  res.end();
+})
+app.get('/hi/:name', (req, res) => {
+  const { name } = req.params;
+  res.cookie('visitor_name', name);
+  res.send('your name is saved!')
 })
 
-// app.get('/hello/new-user', (req, res) => {
-//   console.log('add new user');
-// });
 
-
-// app.get('/hello/:name', (req, res) => {
-//   console.log('hello ' + req.params.name);
-// });
 
 
