f6e424384833d49fbd450c49913791ef981293f8
kierepkae <kierepkaedyta@gmail.com>
2019-10-20 13:48:26 +0100

    send file with jpg in /logo path

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 app.listen(3000, () => {
   console.log('listening . . . ');
 });
 
-app.get('/google', (req, res) => {
-  res.redirect('https://google.com', 301);
+app.get('/', (req, res) => {
+  res.send(`<!DOCTYPE html>
+<html>
+<body>
+<img src='/logo'>
+</body>
+</html>
 
+`
+  )
+});
 
+app.get('/logo', (req, res) => {
+  const fileName = path.join(__dirname + 'static/cat.jpg')
+  res.sendFile(fileName);
+  res.end();
 })
-
 
 // app.get('/hello/new-user', (req, res) => {
 //   console.log('add new user');
