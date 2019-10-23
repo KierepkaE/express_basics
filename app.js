078ae9daaf54e7b9f95528d8b7d70ee5e51cb2a5
kierepkae <kierepkaedyta@gmail.com>
2019-10-23 10:36:43 +0100

    add and use cookie parser in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const app = express();
 app.listen(3000, () => {
@@ -7,4 +8,5 @@ app.listen(3000, () => {
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'static')));
+app.use(cookieParser());
 
