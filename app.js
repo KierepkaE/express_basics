b3ae4c14c9a7aaf02cf0337daa98286af23ba171
kierepkae <kierepkaedyta@gmail.com>
2019-10-23 10:45:30 +0100

    save cookies for / and /hi/:name paths

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(cookieParser());
 
+
+app.get('/', (req, res) => {
+  const { visitor_name } = req.params;
+  if (visitor_name) {
+    res.send(`Hello ${visitor_name}`)
+  } else {
+    res.send('Do we know each other?');
+  }
+})
+app.get('/hi/:name', (req, res) => {
+  const { name } = req.params;
+  res.cookie('visitor_name', name);
+  res.send('Saved!');
+})
+
